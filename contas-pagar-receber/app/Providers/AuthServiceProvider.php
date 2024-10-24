9863a6ead6a25a88f92803a5446b3b671628b6e2
Leandro Damasio <ldamasio@gmail.com>
2024-10-24 11:49:20 -0300

    feat(auth): Register ContaPolicy in AuthServiceProvider

--- contas-pagar-receber/app/Providers/AuthServiceProvider.php
+++ contas-pagar-receber/app/Providers/AuthServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+// use Illuminate\Support\ServiceProvider;
+use App\Models\Conta;
+use App\Policies\ContaPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,11 +17,15 @@ class AuthServiceProvider extends ServiceProvider
         //
     }
 
+    protected $policies = [
+        Conta::class => ContaPolicy::class,
+    ];
+
     /**
      * Bootstrap services.
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
     }
 }
