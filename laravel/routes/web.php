7ea2f89b862127632ad30893c9644d1483f31d74
Leandro Damasio <ldamasio@gmail.com>
2024-10-24 06:48:27 -0300

    feat(auth): require authentication for ContaController routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContaController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('contas', ContaController::class);
+});
 
 Route::get('/', function () {
     return view('welcome');
