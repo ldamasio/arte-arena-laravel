3a89ed908ae127d985c05636e057a8fd075ae80f
Leandro Damasio <ldamasio@gmail.com>
2024-10-24 04:44:35 -0300

    feat(policy): Config ContaPolicy to verify permissions

--- laravel/app/Policies/ContaPolicy.php
+++ laravel/app/Policies/ContaPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Conta;
 
 class ContaPolicy
 {
@@ -12,5 +13,25 @@ class ContaPolicy
     public function __construct()
     {
         //
+    }
+
+    public function viewAny(User $user) {
+        return $user->role === 'admin';
+    }
+    
+    public function view(User $user, Conta $conta) {
+        return $user->role === 'admin' || $user->id === $conta->user_id;
+    }
+    
+    public function create(User $user) {
+        return $user->role === 'user';
+    }
+    
+    public function update(User $user, Conta $conta) {
+        return $user->id === $conta->user_id;
+    }
+    
+    public function delete(User $user, Conta $conta) {
+        return $user->id === $conta->user_id;
     }
 }
