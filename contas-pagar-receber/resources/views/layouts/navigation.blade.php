18a74d86077939a4714a448a043985bcb2d49e0b
Leandro Damasio <ldamasio@gmail.com>
2024-10-25 06:07:13 -0300

    feat(navigation): Add new links to main navigation

--- contas-pagar-receber/resources/views/layouts/navigation.blade.php
+++ contas-pagar-receber/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ asset('arte-arena.webp') }}" alt="Arte Arena" class="block h-9 w-auto">
                     </a>
                 </div>
 
@@ -14,6 +14,24 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('contas.index')" :active="request()->routeIs('dashboard')">
+                        Listas Contas
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('criar.conta')" :active="request()->routeIs('dashboard')">
+                        Adicionar Conta
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('criar.conta')" :active="request()->routeIs('dashboard')">
+                        Relatórios
                     </x-nav-link>
                 </div>
             </div>
@@ -43,7 +61,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -89,7 +107,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
