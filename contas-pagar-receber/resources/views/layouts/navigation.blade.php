f1948db2b0cef939b5a84ec190380c4efc4148da
Leandro Damasio <ldamasio@gmail.com>
2024-10-27 03:48:44 -0300

    fix(navigation): Reorder navigation items for better user experience

--- contas-pagar-receber/resources/views/layouts/navigation.blade.php
+++ contas-pagar-receber/resources/views/layouts/navigation.blade.php
@@ -12,14 +12,8 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('contas.index')" :active="request()->routeIs('dashboard')">
-                        Listas Contas
+                        Listar Contas
                     </x-nav-link>
                 </div>
 
